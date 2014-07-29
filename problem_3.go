b363cd64a557b5793f46d57fe011a2cccb80f932
Alexandru Geana <alex.geana@erepubliklabs.com>
2014-07-29 11:07:33 +0300

    Problem 3
    Solution for problem 3

---
+++ problem_3.go
@@ -0,0 +1,32 @@
+/*
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	const n int64 = 13195
+	var newnum int64 = n
+	var largestFact int64 = 0
+
+	var counter int64 = 2
+
+	for counter*counter <= newnum {
+		if newnum%counter == 0 {
+			newnum = newnum / counter
+			largestFact = counter
+
+		} else {
+			counter++
+		}
+	}
+
+	if newnum > largestFact {
+		largestFact = newnum
+	}
+
+	fmt.Println("Largest factor: ", largestFact)
+}
